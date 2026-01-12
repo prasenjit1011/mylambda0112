0fbff912e31de50c4b6d0dcfd0342d51d5b9db34
Prasenjit Aluni <prasenjit.aluni@iqvia.com>
2026-01-12 20:29:56 +0530

    Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 exports.handler = async (event) => {
-  console.log('event', JSON.stringify(event));
+  console.log('Received event:', JSON.stringify(event));
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello from Lambda', input: event }),
